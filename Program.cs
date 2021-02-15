dd3fff93ebdc1531fa4834504a336b17837ec876
lennyanime <47304773+lennyanime@users.noreply.github.com>
2021-02-14 23:39:02 -0500

    básico

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Curso_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*$ para interpolar ...{variable|constante}*/
+            /*const -> para declarar constantes*/
+            /*puedo mostrar el valor de una constante indicando su posición dentro de una cadena ->{0}*/
+
+
+        }
+    }
+}
